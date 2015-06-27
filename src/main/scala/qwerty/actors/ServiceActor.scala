727e17492e57ac27939347b0743fbb1587b702ce
KasonChan <kasonl.chan@gmail.com>
2015-06-27 01:05:36 -0500

    Add logging, handle exception and timeout; increase timeout

--- src/main/scala/qwerty/actors/ServiceActor.scala
+++ src/main/scala/qwerty/actors/ServiceActor.scala
@@ -1,7 +1,12 @@
 package qwerty.actors
 
 import akka.actor.{Actor, ActorLogging}
+import qwerty.models.Messages
+import qwerty.protocols.MessagesProtocol._
 import qwerty.services.Service
+import spray.http.{HttpResponse, StatusCodes, _}
+import spray.json._
+import spray.routing.ExceptionHandler
 
 /**
  * Created by kasonchan on 6/26/15.
@@ -19,6 +24,24 @@ class ServiceActor extends Actor with ActorLogging with Service {
   // This actor only runs our route, but you could add
   // other things here, like request stream processing
   // or timeout handling
-  def receive = runRoute(route)
+  def receive = handleTimeouts orElse runRoute(route)
+
+  def handleTimeouts: Receive = {
+    case Timedout(x: HttpRequest) =>
+      val messages = Messages(Seq("Timeout")).toJson
+      log.info(messages.compactPrint)
+      sender() ! HttpResponse(StatusCodes.InternalServerError,
+        HttpEntity(ContentTypes.`application/json`, messages.prettyPrint))
+  }
+
+  implicit def handleExceptions: ExceptionHandler =
+    ExceptionHandler {
+      case e: Exception =>
+        requestUri { uri =>
+          val messages = Messages(Seq(e.toString)).toJson
+          log.warning(messages.compactPrint, uri)
+          complete(StatusCodes.InternalServerError, messages.prettyPrint)
+        }
+    }
 
 }
--- src/main/scala/qwerty/services/Service.scala
+++ src/main/scala/qwerty/services/Service.scala
@@ -3,9 +3,10 @@ package qwerty.services
 import qwerty.controllers.Users
 import qwerty.models.Messages
 import qwerty.protocols.MessagesProtocol._
-import spray.http._
+import spray.http.{HttpResponse, StatusCodes, _}
 import spray.json._
 import spray.routing._
+import spray.util.LoggingContext
 
 /**
  * Created by ka-son on 6/25/15.
@@ -13,12 +14,13 @@ import spray.routing._
 // This trait defines the qwerty.service behavior independently from the qwerty.service actor
 trait Service extends HttpService with Users {
 
-  def route = {
+  def route(implicit log: LoggingContext) = {
 
     path("") {
       get {
         complete {
           val messages = Messages(Seq("Welcome to Qwerty API")).toJson
+          log.info(messages.compactPrint)
           HttpResponse(
             StatusCodes.OK,
             HttpEntity(ContentTypes.`application/json`, messages.prettyPrint)
@@ -31,6 +33,7 @@ trait Service extends HttpService with Users {
           get {
             complete {
               val messages = Messages(Seq("PathPrefix pathEnd get")).toJson
+              log.info(messages.compactPrint)
               HttpResponse(
                 StatusCodes.NotFound,
                 HttpEntity(ContentTypes.`application/json`, messages.prettyPrint))
@@ -39,6 +42,7 @@ trait Service extends HttpService with Users {
             post {
               complete {
                 val messages = Messages(Seq("PathPrefix pathEnd post")).toJson
+                log.info(messages.compactPrint)
                 HttpResponse(
                   StatusCodes.NotFound,
                   HttpEntity(ContentTypes.`application/json`, messages.prettyPrint))
@@ -49,6 +53,7 @@ trait Service extends HttpService with Users {
             get {
               complete {
                 val messages = Messages(Seq("PathPrefix users get")).toJson
+                log.info(messages.compactPrint)
                 HttpResponse(
                   StatusCodes.OK,
                   HttpEntity(ContentTypes.`application/json`, messages.prettyPrint))
@@ -60,7 +65,7 @@ trait Service extends HttpService with Users {
         (get | post | put | delete | patch) {
           complete {
             val messages = Messages(Seq("Not found")).toJson
-            //            log.info(messages.compactPrint)
+            log.info(messages.compactPrint)
             HttpResponse(
               StatusCodes.NotFound,
               HttpEntity(ContentTypes.`application/json`, messages.prettyPrint))
